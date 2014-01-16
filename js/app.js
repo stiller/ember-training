cede366c5484a3c3c1e33676e68a8c3a9ed7ef66
Joachim Nolten <joachim@nolten.org>
2014-01-16 15:29:16 +0100

    step 1 passing

--- js/app.js
+++ js/app.js
@@ -13,26 +13,4 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
-App.AlbumRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
-  }
-});
-
-Ember.Handlebars.helper('format-duration', function(seconds) {
-  var minutes = Math.floor(seconds/60);
-  var remainingSeconds = seconds % 60;
-
-  var result = '';
-  if (remainingSeconds < 10) {
-    result = "0";
-  }
-
-  result += String(remainingSeconds);
-
-  result = minutes + ":" + result;
-
-  return result;
-});
-
 })();
